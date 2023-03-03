4b608d9dac4ec13a1a5a4a263870b5e4b66468fc
Robi McAndrew <robin@mcandrew.email>
2023-03-03 19:57:49 +1100

    Initial commit

---
+++ test/aax-mp3-rss.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AaxMp3Rss from '../lib/aax-mp3-rss-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aax-mp3-rss-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AaxMp3Rss.AaxMp3RssStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
